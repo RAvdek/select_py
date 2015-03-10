648d5bb150119b36b84fe676ef624ac7f7a054d7
RAvdek <russell.avdek@gmail.com>
2015-03-09 23:03:27 -0700

    Updated path location with os.system

---
+++ lib/sql_server.py
@@ -0,0 +1,55 @@
+from time import time
+from os import system as shell
+import os
+from base import Base
+
+class SQLServer(Base):
+	""" SQLServer(query,data_resource):
+		query: SQLServer query in string format
+		data_resource: Server name
+
+	Access warning: Requires Windows authentication. Designed
+	to be executed via Cygwin on a Windows machine.
+	"""
+
+	CMD = "cmd /c sqlcmd -S {resource} -i {infile} -s '\t' -W -o {outfile}"
+
+	def _set_up(self):
+		""" No setup required """
+		pass
+
+	def _clean_line(self,line):
+		data = line.split('\t')
+		data = '\t'.join(map(lambda z: z.strip(), data))
+		if data == '' or ('--' in data) or ('rows affected' in data):
+			return ''
+		elif "Changed database context to" in line:
+			return ''
+		else: 
+			return data + '\n'
+
+	def _tear_down(self):
+		""" Clean up the junk in the output """
+		# move data to a buffer file
+		buffer_address = "__sqlserver_buffer_{0}.tsv".format("_".join(str(time()).split(".")))
+		os.rename(self.output_file, buffer_address)
+		job_failed = False
+		try:
+			buffer_file = open(buffer_address, mode = 'r')
+			target_file = open(self.output_file, mode = 'w')
+			# Hacky workaround to catch bugs in SQL
+			for line in buffer_file:
+				if ("Msg " in line) & ("Level " in line) & ("State " in line):
+					job_failed = True
+					error_msg = line
+					raise Exception("SQL script contains the following error:\n{0}"\
+					.format(error_msg))
+				else: 
+					target_file.write(self._clean_line(line))
+		finally:
+			buffer_file.close()
+			target_file.close()
+			os.remove(buffer_address)
+			if job_failed == True:
+				os.remove(self.target_file)
+
---
+++ tests/base_test.py
@@ -0,0 +1,40 @@
+import unittest
+import time
+from os import system as shell
+from select_py import Base
+
+""" 
+In order to run this thing, we need to execute the test 'above'
+the highest directory as a module:
+
+http://stackoverflow.com/questions/1918539/can-anyone-explain-pythons-relative-imports
+https://www.python.org/dev/peps/pep-0328/#rationale-for-relative-imports
+
+$ pwd
+/cygdrive/e
+$ ls select_py/
+__init__.py  __init__.pyc  select_py  tests
+$ python -m select_py.tests.base_test
+"""
+
+class BaseTest(unittest.TestCase):
+	
+	def setUp(self):
+		shell("mkdir _test_dir_")
+		shell("echo * > _test_dir_/_test_file_")
+
+	def tearDown(self):
+		shell("rm -rd _test_dir_")
+
+	def test_shell_exec(self):
+		base = Base('a','b','c')
+		data = base._shell_exec("echo _test_dir_/*")
+		self.assertEqual(data, "_test_dir_/_test_file_")
+
+	def test_output_summary(self):
+		base = Base('a','b','c')
+		data = base._output_summary("_test_dir_/_test_file_")
+		self.assertEqual(data['line count'],1)
+
+if __name__ == "__main__":
+	unittest.main()
